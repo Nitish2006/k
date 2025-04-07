8103b7b7a1fc7c78f7e52e29203facfd88c7702e
Nitish2006 <nitishvellanki@gmail.com>
2025-04-08 05:06:56 +0530

    Update Event.js

--- models/Event.js
+++ models/Event.js
@@ -1,12 +1,13 @@
-import mongoose from "mongoose";
- const eventSchema = new mongoose.Schema({
-    name: { type: String, rquired:true},
-    date:{type:Date, rquired:true},
-    coordinatorName:{ type: String, required: true},
-    coordinatorContact:{type: String, required:true},
-    poster: { type: String, required:true},
-    phonepeScreenchot:{ type: String, requred:true},
-    whatsappLink: { type:String,required: true},
-    createdAt: { type: Date, default: Date.now},
- });
- export default mongoose.model('Event',eventSchema);
+import mongoose from 'mongoose';
+
+const eventSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    date: { type: Date, required: true },
+    coordinatorName: { type: String, required: true },
+    coordinatorContact: { type: String, required: true },
+    poster: { type: String, required: true },
+    phonepeScreenshot: { type: String }, // URL to the uploaded file
+    whatsappLink: { type: String, required: true }
+});
+
+export default mongoose.model('Event', eventSchema);
